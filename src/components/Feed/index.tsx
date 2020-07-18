be872a34aa32e6ae5e71f2623e7272172cb5569f
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-07-17 22:07:01 -0300

    feed file

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Tweet from '../Tweet';
+
+import { Container, Tab, Tweets } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+    <Container>
+      <Tab>Tweets</Tab>
+
+      <Tweets>
+        <Tweet />
+      </Tweets>
+    </Container>
+  );
+};
+
+export default Feed;
