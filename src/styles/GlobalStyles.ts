174b182ae8d3691c15ebb506b0a20991248bebdb
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-07-17 21:58:52 -0300

    project styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    color: var(--white);
+  }
+
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    width: 100%;
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  html {
+    background: var(--primary);
+  }
+
+  :root {
+    --primary: #000;
+    --secondary: #15181c;
+    --search: #202327;
+    --white: #d9d9d9;
+    --gray: #7a7a7a;
+    --outline: #2f3336;
+    --retweet: #00c06b;
+    --like: #e8265e;
+    --twitter: #33a1f2;
+    --twitter-dark-hover: #011017;
+    --twitter-light-hover: #2c8ed6;
+  }
+`;
---
+++ src/styles/Icons.ts
@@ -0,0 +1,16 @@
+export {
+  Home,
+  Notifications,
+  Email,
+  FavoriteBorder,
+  BookmarkBorder,
+  ListAlt,
+  KeyboardArrowDown,
+  LocationOn,
+  Search,
+} from 'styled-icons/material-outlined';
+export { Person, Cake, Favorite } from 'styled-icons/material';
+export { ArrowLeft } from 'styled-icons/heroicons-solid';
+export { Hashtag, Retweet } from 'styled-icons/fa-solid';
+export { Twitter } from 'styled-icons/fa-brands';
+export { Chat } from 'styled-icons/heroicons-outline';
