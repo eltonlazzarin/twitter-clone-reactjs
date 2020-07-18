5b1ef72f38af0646f1a76f124f96584c6a4513bc
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-07-17 22:01:58 -0300

    main project file

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
