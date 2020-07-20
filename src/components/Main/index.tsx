4e060fe53f3147a8aaa763986fc287cf5099025e
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-07-19 22:19:55 -0300

    main component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import ProfilePage from '../ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Elton Lazzarin</strong>
+          <span>432 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      <ProfilePage />
+
+      <BottomMenu>
+        <HomeIcon className="active" />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
